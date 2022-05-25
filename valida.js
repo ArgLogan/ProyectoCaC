38e79d1038abd126f99809af2287853804a05aa0
JJRoma <jj_roma@yahoo.ccom.ar>
2022-05-25 15:56:16 -0300

    005

--- valida.js
+++ valida.js
@@ -2,16 +2,24 @@
 function validar(){
     let mail = document.contacto.email.value;
     let myRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let flag = false;
     let mensaje = ""
+    mensaje = document.contacto.mensaje.value;
+
     if( !myRegex.test(mail) ){
         alert("email invalido");
+    }else{
+        if(document.contacto.nombre.value == ""){
+            alert("debe ingresar un mombre");
+        }else{
+            if(mensaje.length < 30 ){
+                alert("esccriba un mensaje de al menos 30 caracateres");
+            }else{
+                alert("Su mensaje fue enviado")
+            }
+        }
     }
 
-    if(document.contacto.nombre.value == ""){
-        alert("debe ingresar un mombre");
-    }
-    mensaje = document.contacto.mensaje.value;
-    if(document.contacto.mensaje.value == ""){
-        alert("esccriba un mensaje de al menos 30 caracateres");
-    }
+    
+   
 }
